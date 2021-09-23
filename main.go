f021467a8f68e63fcaddb798f6c2e0db5992900f
erickstm87 <dev@terickson.io>
2021-09-22 23:57:45 -0600

    updated with no entries

--- main.go
+++ main.go
@@ -4,26 +4,39 @@ import (
 	"fmt"
 	"net/http"
 	"github.com/PuerkitoBio/goquery"
+	"os"
 )
 
 func main() {
-	resp, err := http.Get("https://www.pinkbike.com/buysell/list/?region=3&q=hightower&framesize=9,11,12,17,18,20,21,22,16,19,24,25,26,28,29")
-	if err != nil {
-		errorHandler(err)
-	}
-
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	if err != nil {
-		errorHandler(err)
-	}
-	doc.Find(".uImage").Each(func(i int, s *goquery.Selection) {
-		title, _ := s.Children().Attr("href")
-		fmt.Println(title, i)
+	bikePage := getHtmlBody("https://www.pinkbike.com/buysell/list/?region=3&q=hightower&framesize=9,11,12,17,18,20,21,22,16,19,24,25,26,28,29")
+	found := false
+	
+	bikePage.Find(".uImage").Each(func(i int, s *goquery.Selection) {
+		found = true
+		link, _ := s.Children().Attr("href")
+		fmt.Println(link)
 	})
+	if(!found) {
+		fmt.Println("no entries were found")
+		os.Exit(1)
+	}
 }
 
 func errorHandler(err error) {
 	fmt.Println("there was an error: ", err)
 }
 
+func getHtmlBody(url string) *goquery.Document {
+	resp, err := http.Get(url)
+	if err != nil {
+		errorHandler(err)
+	}
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		errorHandler(err)
+	} 
+	return doc
+}
+
 // https://www.pinkbike.com/buysell/list/?region=3&q=hightower&framesize=9,11,12,17,18,20,21,22,16,19,24,25,26,28,29
+// https://www.pinkbike.com/buysell/list/?region=3&q=hightower&framesize=9,11,12,17,18,20,21,22
